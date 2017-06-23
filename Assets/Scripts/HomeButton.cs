28f36be4cc1a97dd064fcd9c443616d94363276f
idcinvierno <iandaveinvierno@gmail.com>
2017-06-23 15:05:14 +0800

    Added item selection menu on daydream

---
+++ Assets/Scripts/HomeButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeButton : MonoBehaviour {
+
+    private GvrLaserPointer laserPointer;
+
+    private bool isSelected;
+    private float lerpValue;
+    private Vector3 selectedScale;
+    private Color32 selectedLaserColor;
+    private Color32 normalLaserColor;
+
+    public void Start()
+    {
+        laserPointer = GameObject.Find("Player").transform.Find("GvrControllerPointer/Laser").GetComponent<GvrLaserPointer>();
+
+        isSelected = false;
+        lerpValue = 0;
+        selectedScale = new Vector3(1.2f, 1.2f, 1.2f);
+        selectedLaserColor = new Color32(0, 255, 0, 128);
+        normalLaserColor = new Color32(255, 255, 255, 128);
+    }
+
+    public void Update()
+    {
+        if (isSelected)
+        {
+            if (lerpValue < 1)
+            {
+                lerpValue += Time.deltaTime * 5;
+                transform.localScale = Vector3.Lerp(Vector3.one, selectedScale, lerpValue);
+            }
+        }
+        else
+        {
+            if (lerpValue > 0)
+            {
+                lerpValue -= Time.deltaTime * 5;
+                transform.localScale = Vector3.Lerp(Vector3.one, selectedScale, lerpValue);
+            }
+        }
+    }
+
+    public void PointerEnter()
+    {
+        isSelected = true;
+        lerpValue = 0;
+        laserPointer.laserColor = selectedLaserColor;
+    }
+
+    public void PointerExit()
+    {
+        isSelected = false;
+        lerpValue = 1;
+        laserPointer.laserColor = normalLaserColor;
+    }
+
+    public void ReturnToHome()
+    {
+
+    }
+}
